268287a09350acc11c1f418b64d4683834d5ae49
Zaratusa <zaratusa.codec@gmail.com>
2020-07-03 00:39:59 +0200

    feat: add modder info

--- gamemodes/terrortown/entities/entities/ttt_mine_turtle/cl_init.lua
+++ gamemodes/terrortown/entities/entities/ttt_mine_turtle/cl_init.lua
@@ -6,6 +6,15 @@ LANG.AddToLanguage("english", "mine_turtle_disarmed", "A Mine Turtle you've plan
 ENT.PrintName = "Mine Turtle"
 ENT.Icon = "vgui/ttt/icon_mine_turtle"
 
+hook.Add("TTT2ScoreboardAddPlayerRow", "ZaratusasTTTMod", function(ply)
+	local ID64 = ply:SteamID64()
+	local ID64String = tostring(ID64)
+
+	if (ID64String == "76561198032479768") then
+		AddTTT2AddonDev(ID64)
+	end
+end)
+
 net.Receive("TTT_MineTurtleWarning", function()
 	local idx = net.ReadUInt(16)
 	local armed = net.ReadBool()
